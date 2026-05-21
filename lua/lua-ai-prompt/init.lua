194a31314929daf6a495748eebedd605cc54ef8d
tdecelle <tdecelle@gmail.com>
2026-05-21 00:23:11 +0000

    plugin?

--- lua/lua-ai-prompt/init.lua
+++ lua/lua-ai-prompt/init.lua
@@ -1,7 +1,14 @@
 local M = {}
 
-M.hello = function()
-  print("Hello, World!")
+function M.hello()
+	print("Hello, World!")
+end
+
+function M.setup(opts)
+	opts = opts or {}
+
+	-- Create the user command
+	vim.api.nvim_create_user_command("HelloWorld", M.say_hello, {})
 end
 
 return M
